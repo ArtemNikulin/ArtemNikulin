f21ff7b04735667ac8590c51e798aa32999312ec
ArtemNikulin <32359845+ArtemNikulin@users.noreply.github.com>
2018-06-03 21:32:13 +0300

    Add files via upload

---
+++ Quack/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+   public static void main(String[] args) {
+        Duck a = new SimpleDuck();
+        a.performQuack();
+    }
+}
+abstract class Duck{
+    IQuackBehav qb;
+    void performQuack()
+    {
+        qb.quack();
+    }
+}
+interface IQuackBehav{
+    void quack();
+}
+interface ISwimBehav{
+    void quack();
+}
+class QuackebleBehaviour implements IQuackBehav {
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
+class SimpleDuck extends Duck{
+   SimpleDuck(){
+        qb = new QuackebleBehaviour();
+    }
+
+}
